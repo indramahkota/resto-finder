c7343ef7d2e37697a99c1ddc4d3b3eba571c75ed
Indra Mahkota <indramahkota1@gmail.com>
2020-08-27 20:51:05 +0700

    fix header humburger button

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -2,7 +2,7 @@
  * @author Indra Mahkota
  * @email indramahkota1@gmail.com
  * @create date 2020-08-26 21:36:00
- * @modify date 2020-08-27 13:44:29
+ * @modify date 2020-08-27 20:40:06
  * @desc [description]
  */
 import '../styles/Header.css';
@@ -12,6 +12,10 @@ import TemplateFactory from './utilities/TemplateFactory.js';
 const App = () => {
     const root = document.getElementById('root');
     root.appendChild(TemplateFactory.create(Header));
+
+    //header humburger button
+    const hdrHumburger = document.getElementById('header-menu-hamburger');
+    hdrHumburger.addEventListener('click', () => hdrHumburger.classList.toggle("change"));
 }
 
 export default App;
