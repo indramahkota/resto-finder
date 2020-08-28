b3b77a8b2a0736c5155a62dcbd07b5b8997c6767
Indra Mahkota <indramahkota1@gmail.com>
2020-08-28 20:34:23 +0700

    remove old coding style

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
  * @author Indra Mahkota
  * @email indramahkota1@gmail.com
  * @create date 2020-08-26 21:34:28
- * @modify date 2020-08-28 17:30:30
+ * @modify date 2020-08-28 20:33:43
  * @desc [description]
  */
-import './styles/Index.css';
+import './styles/Index.scss';
 import App from "./scripts/App.js";
 
 document.addEventListener("DOMContentLoaded", App);
--- src/scripts/App.js
+++ src/scripts/App.js
@@ -2,57 +2,25 @@
  * @author Indra Mahkota
  * @email indramahkota1@gmail.com
  * @create date 2020-08-26 21:36:00
- * @modify date 2020-08-28 17:32:09
+ * @modify date 2020-08-28 19:59:11
  * @desc [description]
  */
-/* import '../styles/Root.css';
-import '../styles/Header.css';
-import '../styles/Hero.css';
-import '../styles/Content.css';
-import '../styles/Footer.css';
-import '../styles/NavigationAside.css';
-import Header from '../templates/Header.html';
-import Hero from '../templates/Hero.html';
-import Content from '../templates/Content.html';
-import Footer from '../templates/Footer.html';
-import NavigationAside from '../templates/NavigationAside.html';
-import TemplateFactory from './utilities/TemplateFactory.js'; */
 
 /* Struktur Element
     body
         noscript
         #root
-            header
-            main
-                .hero
-                .content
-                    article
-                footer
-            aside (position absolute terhadap root)
-                .overlay
-                .sidebar
+            header (app-bar)---------------------|-----> satu komponen
+            main (main-container)----------------|-----> satu komponen
+                .hero (hero-element)-------------|----------------------> imported komponen
+                .content (article-container)-----|----------------------> imported komponen
+                    article (card-element)-------|----------------------> imported komponen
+                footer (foot-bar)----------------|----------------------> imported komponen
+            aside (side-bar)---------------------|-----> satu komponen
 */
+
 const App = () => {
-    /* const root = document.getElementById('root');
-    root.appendChild(TemplateFactory.create(Header));
 
-    const main = document.createElement('main');
-    root.appendChild(main);
-
-    //referensi struktur https://www.dicoding.com/academies/123/tutorials/4150
-    main.appendChild(TemplateFactory.create(Hero));
-    main.appendChild(TemplateFactory.create(Content));
-    main.appendChild(TemplateFactory.create(Footer));
-
-    //add navigation aside diluar main
-    const aside = document.createElement('aside');
-    root.appendChild(aside);
-
-    aside.appendChild(TemplateFactory.create(NavigationAside));
-
-    //header humburger (button)
-    const hdrHumburger = document.getElementById('header-hamburger');
-    hdrHumburger.addEventListener('click', () => hdrHumburger.classList.toggle("change")); */
 }
 
 export default App;
