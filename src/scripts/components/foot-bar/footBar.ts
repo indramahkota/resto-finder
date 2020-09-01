acbc3345c77096a40f08f9cc3ba29ad84ae3e9c6
Indra Mahkota <indramahkota1@gmail.com>
2020-09-01 20:48:00 +0700

    menambahkan foot bar

---
+++ src/scripts/components/foot-bar/footBar.ts
@@ -0,0 +1,55 @@
+/**
+ * @author Indra Mahkota
+ * @email indramahkota1@gmail.com
+ * @create date 2020-09-01 17:49:40
+ * @modify date 2020-09-01 17:50:42
+ * @desc [description]
+ */
+
+/*
+// Penggunaan
+import {html, render} from 'lit-html';
+import AppConfig from './scripts/globals/appConfig';
+
+render(html`
+    <foot-bar
+        text=${AppConfig.TEXT_FOOTER}
+    </foot-bar>
+`, document.body); */
+
+import { html } from 'lit-html';
+import { customElement, property } from 'lit-element';
+
+import CommonElement from '../_base_/commonElement';
+import AppConfig from '../../globals/appConfig';
+
+import style from './foot-bar.scss';
+
+@customElement('foot-bar')
+class FootBar extends CommonElement {
+    @property({ type: String, attribute: true })
+    text: string;
+
+    static get styles() {
+        return [...super.styles, style];
+    }
+
+    constructor() {
+        super();
+        this.text = AppConfig.TEXT_FOOTER;
+    }
+
+    render() {
+        return html`
+            <footer>
+                <p>${this.text}</p>
+            </footer>
+        `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'foot-bar': FootBar;
+    }
+}
