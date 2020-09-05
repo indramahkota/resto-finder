d09901da09aa98e1b1ae7b0b653b2db5faf46652
Indra Mahkota <indramahkota1@gmail.com>
2020-09-05 12:33:24 +0700

    fix rating element

---
+++ src/scripts/components/rating-element/ratingElement.ts
@@ -0,0 +1,63 @@
+/**
+ * @author Indra Mahkota
+ * @email indramahkota1@gmail.com
+ * @create date 2020-09-05 09:57:09
+ * @modify date 2020-09-05 09:57:09
+ * @desc [description]
+ */
+
+import { html } from 'lit-html';
+import { customElement, property } from 'lit-element';
+
+import "../hero-element/heroElement";
+import "../resto-container/restoContainer";
+import CommonElement from '../_base_/commonElement';
+import AppIcons from '../../globals/appIcons';
+import Utils from '../../globals/appUtilities';
+import { Icon } from '@fortawesome/fontawesome-svg-core';
+
+import style from './rating-element.scss';
+
+@customElement('rating-element')
+export default class RatingElement extends CommonElement {
+    @property({ type: Number, attribute: true })
+    rating: number;
+
+    private _starIcon: Icon;
+
+    static get styles() {
+        return [...super.styles, style];
+    }
+
+    constructor() {
+        super();
+        this.rating = 0;
+        this._starIcon = AppIcons.STAR;
+    }
+
+    render() {
+        return html`
+            <div class="rating__container">
+                ${Utils.genSVG(this._starIcon)}
+                ${Utils.genSVG(this._starIcon)}
+                ${Utils.genSVG(this._starIcon)}
+                ${Utils.genSVG(this._starIcon)}
+                ${Utils.genSVG(this._starIcon)}
+
+                <div class="gold__rating__item" style="width: ${(this.rating/5)*100}%;">
+                    ${Utils.genSVG(this._starIcon)}
+                    ${Utils.genSVG(this._starIcon)}
+                    ${Utils.genSVG(this._starIcon)}
+                    ${Utils.genSVG(this._starIcon)}
+                    ${Utils.genSVG(this._starIcon)}
+                </div>
+            </div>
+        `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'rating-element': RatingElement;
+    }
+}
