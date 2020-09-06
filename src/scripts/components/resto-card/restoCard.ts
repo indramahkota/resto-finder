a5c81cb287e6628ddeabf9e866526ef0d1543bed
Indra Mahkota <indramahkota1@gmail.com>
2020-09-06 14:19:58 +0700

    typo

--- src/scripts/components/resto-card/restoCard.ts
+++ src/scripts/components/resto-card/restoCard.ts
@@ -1,5 +1,4 @@
-
-import "../rating-element/ratingElement";/**
+/**
  * @author Indra Mahkota
  * @email indramahkota1@gmail.com
  * @create date 2020-09-05 23:57:18
@@ -12,6 +11,7 @@ import { customElement, property } from 'lit-element';
 import { ifDefined } from 'lit-html/directives/if-defined';
 
 import "../hero-element/heroElement";
+import "../rating-element/ratingElement";
 import CommonElement from '../_base_/commonElement';
 
 import style from './resto-card.scss';
