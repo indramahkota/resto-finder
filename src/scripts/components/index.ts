da2d9d14ca816990ad9b5bbafde2cecd450c8785
Indra Mahkota <indramahkota1@gmail.com>
2020-09-08 00:07:01 +0700

    menambahkan comments

--- src/scripts/components/index.ts
+++ src/scripts/components/index.ts
@@ -1,4 +1,11 @@
+/* AppBar component */
 import './app-bar/appBar';
+
+/* AppContent component */
 import './app-content/appContent';
+
+/* MyProfile component */
 import './my-profile/myProfile';
+
+/* FootBar component */
 import './foot-bar/footBar';
