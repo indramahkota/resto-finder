28c080f20052eb84cf1f5925fb07f5344366035b
Indra Mahkota <indramahkota1@gmail.com>
2020-09-08 18:19:13 +0700

    menambahkan fungsi idb

---
+++ src/scripts/data/sources/local/database/localdatabase.ts
@@ -0,0 +1,48 @@
+import { openDB, DBSchema } from "idb";
+import { IRestaurant } from "../../../../interfaces/interfaces";
+
+interface RestoDatabase extends DBSchema {
+    restaurants: {
+        value: IRestaurant;
+        key: string;
+        indexes: { 'by-id': string, 'by-city': string, 'by-rating': number };
+    };
+}
+
+const getDatabase = async () => {
+    return await openDB<RestoDatabase>("resto-finder-database", 1, {
+        upgrade: db => {
+            const store = db.createObjectStore("restaurants", { keyPath: "id" });
+            store.createIndex("by-id", "id");
+            store.createIndex("by-city", "city");
+            store.createIndex("by-rating", "rating");
+        }
+    });
+}
+
+export default class LocalDatabase {
+    static async createFavorite(data: IRestaurant) {
+        const db = await getDatabase();
+        return await db.add("restaurants", data);
+    }
+
+    static async getFavoriteById(id: string) {
+        const db = await getDatabase();
+        return await db.get("restaurants", id);
+    }
+
+    static async getAllFavorite() {
+        const db = await getDatabase();
+        return await db.getAll("restaurants");
+    }
+
+    static async updateFavorite(data: IRestaurant) {
+        const db = await getDatabase();
+        return await db.put("restaurants", data);
+    }
+
+    static async deleteFavorite(id: string) {
+        const db = await getDatabase();
+        return await db.delete("restaurants", id);
+    }
+}
