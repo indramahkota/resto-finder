cbdb0ce9b5472440e8e9696ca5513195cf083c76
Indra Mahkota <indramahkota1@gmail.com>
2020-09-12 01:00:03 +0700

    add search bar event hide/show

--- src/scripts/components/search-bar/searchBar.ts
+++ src/scripts/components/search-bar/searchBar.ts
@@ -5,9 +5,58 @@ import CommonElement from '../_base_/commonElement';
 
 @customElement('search-bar')
 export default class SearchBar extends CommonElement {
+    private searchBar: HTMLElement | null = null;
+
+    private current_scroll_position = 0;
+    private last_known_scroll_position = 0;
+    private ticking = false;
+
+    private _onScrollHandler = () => {
+        this.current_scroll_position = window.scrollY;
+
+        window.setTimeout(() => {
+            this.last_known_scroll_position = window.scrollY;
+        }, 150);
+
+        if (!this.ticking) {
+            window.requestAnimationFrame(() => {
+                this.hideOrShowsearchBar();
+                this.ticking = false;
+            });
+            this.ticking = true;
+        }
+    }
+
+    hideOrShowsearchBar(): void {
+        if(this.current_scroll_position < 120) {
+            this.searchBar?.classList.remove('hide');
+            return;
+        }
+        const hidesearchBar = this.current_scroll_position > this.last_known_scroll_position;
+        if(hidesearchBar) {
+            this.searchBar?.classList.add('hide');
+        } else {
+            this.searchBar?.classList.remove('hide');
+        }
+    }
+
+    connectedCallback(): void {
+        super.connectedCallback();
+        window.addEventListener('scroll', this._onScrollHandler, false);
+    }
+
+    disconnectedCallback(): void {
+        window.removeEventListener('scroll', this._onScrollHandler, false);
+        super.disconnectedCallback();
+    }
+
+    firstUpdated(): void {
+        this.searchBar = document.getElementById("search-bar");
+    }
+
     render(): TemplateResult {
         return html`
-            <div class="search__floater">
+            <div id="search-bar" class="search__floater">
                 <div class="search__anchor">
                     <form id="search-form" action="get">
                             <input type="text" class="search__bar" placeholder="Search Resto">
