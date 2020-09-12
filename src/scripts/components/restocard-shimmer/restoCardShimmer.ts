bbfc354f5260a618d1a61e217941a5de2a2abd51
Indra Mahkota <indramahkota1@gmail.com>
2020-09-12 19:49:34 +0700

    init resto card shimmer

---
+++ src/scripts/components/restocard-shimmer/restoCardShimmer.ts
@@ -0,0 +1,30 @@
+import { html, TemplateResult } from 'lit-html';
+import { customElement } from 'lit-element';
+
+import "../resto-card/restoCard";
+import "../hero-element/heroElement";
+
+import CommonElement from '../_base_/commonElement';
+
+@customElement('restocard-shimmer')
+export default class RestoCardShimmer extends CommonElement {
+    render(): TemplateResult {
+        return html`
+            <div class="card br">
+                <div class="wrapper">
+                    <div class="pic animate din"></div>
+                    <div class="comment br animate w80"></div>
+                    <div class="comment br animate"></div>
+                    <div class="comment br animate"></div>
+                </div>
+            </div>
+        `;
+    }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare global {
+    interface HTMLElementTagNameMap {
+        'restocard-shimmer': RestoCardShimmer;
+    }
+}
