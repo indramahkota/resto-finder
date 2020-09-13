af3f7792b0de2cf738aaebdeae1f204ec6aa8b03
Indra Mahkota <indramahkota1@gmail.com>
2020-09-14 01:00:20 +0700

    fix optimize

--- src/scripts/data/sources/remote/remoteDataSource.ts
+++ src/scripts/data/sources/remote/remoteDataSource.ts
@@ -4,8 +4,8 @@ export default class RemoteDataSource {
     static async getAllRestaurant<T>(): Promise<T> {
         const response = await fetch(AppConfig.BASE_URL+'list');
         if (response !== undefined && response.status === 200) {
-            /* #WARNING# Sebaiknya jangan:::Supaya nampak loadingnya aja wkwk */
-            await new Promise(res => setTimeout(res, 1000));
+            /* #WARNING# Sebaiknya jangan pakai setTimeout() */
+            await new Promise(res => setTimeout(res, 500));
             return await Promise.resolve(response.json());
         }
         return await Promise.reject(new Error(`Code: ${response.status}, ${response.statusText}`));
