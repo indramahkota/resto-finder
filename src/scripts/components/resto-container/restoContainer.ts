d6aad1e22f24d8e73421704b40953c205691addc
Indra Mahkota <indramahkota1@gmail.com>
2020-09-13 19:08:32 +0700

    fix resto container supaya gak handle fetch request

--- src/scripts/components/resto-container/restoContainer.ts
+++ src/scripts/components/resto-container/restoContainer.ts
@@ -1,13 +1,11 @@
 import { html, TemplateResult } from 'lit-html';
-import { customElement, property, internalProperty } from 'lit-element';
+import { customElement, property } from 'lit-element';
 
 import "../resto-card/restoCard";
 import "../hero-element/heroElement";
 import "../restocard-shimmer/restoCardShimmer";
 
-import AppConfig from '../../globals/appConfig';
 import CommonElement from '../_base_/commonElement';
-import RemoteDataSource from '../../data/sources/remote/remoteDataSource';
 import { IRestaurants } from '../../interfaces/interfaces';
 
 @customElement('resto-container')
@@ -15,22 +13,8 @@ export default class RestoContainer extends CommonElement {
     @property({ type: String, attribute: true })
     title = 'RESTAURANTS';
 
-    @property({ type: String, attribute: true })
-    url: string = AppConfig.JSON_RESTO_DATA_URL;
-
-    @internalProperty()
-    private _dataJson: IRestaurants | null = null;
-    
-    connectedCallback(): void {
-        super.connectedCallback();
-        /* LocalDataSource.getJsonData<IRestaurants>(this.url)
-            .then(re => this._dataJson = re)
-            .catch(er => console.log(er)); */
-
-        RemoteDataSource.getAllRestaurant<IRestaurants>()
-            .then(re => this._dataJson = re)
-            .catch(er => console.log(er));
-    }
+    @property({ type: Object, attribute: true })
+    data: IRestaurants | null = null;
 
     render(): TemplateResult {
         return html`
@@ -38,8 +22,8 @@ export default class RestoContainer extends CommonElement {
                 <h1 tabindex="0" class="resto__title">${this.title}</h1>
                 <div class="resto__items">
                 ${
-                        this._dataJson !== null ?
-                            this._dataJson.restaurants.map(res => html`
+                        this.data !== null ?
+                            this.data.restaurants.map(res => html`
                                 <resto-card .data=${res}></resto-card>
                             `) : html`
                                 <restocard-shimmer></restocard-shimmer>
--- src/scripts/views/pageHome.ts
+++ src/scripts/views/pageHome.ts
@@ -1,21 +1,43 @@
 import { html, TemplateResult } from 'lit-html';
-import { customElement } from 'lit-element';
+import { customElement, internalProperty } from 'lit-element';
 
 import "../components/hero-element/heroElement";
 import "../components/search-bar/searchBar";
 import "../components/resto-container/restoContainer";
 import "../components/resto-map/restoMap";
 import CommonElement from '../components/_base_/commonElement';
+import RemoteDataSource from '../data/sources/remote/remoteDataSource';
+import { IRestaurants } from '../interfaces/interfaces';
+import EventType from '../globals/eventType';
 
 @customElement('rstf-home')
 export default class PageHome extends CommonElement {
+    @internalProperty()
+    private _restoData: IRestaurants | null = null;
+    
+    connectedCallback(): void {
+        super.connectedCallback();
+
+        RemoteDataSource.getAllRestaurant<IRestaurants>()
+            .then(res => this._restoData = res)
+            .catch(err => {
+                const navClicked = new CustomEvent(EventType.SHOW_TOAST, {
+                    detail: {
+                        message: `Failed fetch data: ${err}`
+                    },
+                    bubbles: true
+                });
+                this.dispatchEvent(navClicked);
+            });
+    }
+
     render(): TemplateResult {
         return html`
             <section id="greeting">
                 <hero-element></hero-element>
             </section>
             <section id="top-resto">
-                <resto-container title="TOP RESTAURANTS"></resto-container>
+                <resto-container title="TOP RESTAURANTS" .data=${this._restoData}></resto-container>
             </section>
             <resto-map></resto-map>
             <search-bar></search-bar>
