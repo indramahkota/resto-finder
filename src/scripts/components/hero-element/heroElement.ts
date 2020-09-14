3e5cceab7b3d28b36cbfcc248b1931448fe4dab2
Indra Mahkota <indramahkota1@gmail.com>
2020-09-14 14:33:05 +0700

    menambahkan parallax sedikit saja

--- src/scripts/components/hero-element/heroElement.ts
+++ src/scripts/components/hero-element/heroElement.ts
@@ -12,10 +12,46 @@ export default class HeroElement extends CommonElement {
     @property({ type: String, attribute: true })
     name = AppConfig.APP_NAME;
 
+    /*  #WARNING# Laggg... */
+    private hero_greeting: HTMLElement | null = null;
+    private current_scroll_position = 0;
+    private ticking = false;
+
+    private _onScrollHandler = () => {
+        this.current_scroll_position = window.scrollY;
+        if (!this.ticking) {
+            window.requestAnimationFrame(() => {
+                this.hideOrShowHeader();
+                this.ticking = false;
+            });
+            this.ticking = true;
+        }
+    }
+
+    hideOrShowHeader(): void {
+        if (this.hero_greeting === null)
+            return;
+        this.hero_greeting.style.transform = `translateY(${this.current_scroll_position / 3}px)`;
+    }
+
+    connectedCallback(): void {
+        super.connectedCallback();
+        window.addEventListener('scroll', this._onScrollHandler, false);
+    }
+
+    disconnectedCallback(): void {
+        window.removeEventListener('scroll', this._onScrollHandler, false);
+        super.disconnectedCallback();
+    }
+
+    firstUpdated(): void {
+        this.hero_greeting = document.getElementById('hero-greeting');
+    }
+
     render(): TemplateResult {
         return html`
             <div class="hero__background">
-                <div class="hero__placeholder">
+                <div id="hero-greeting" class="hero__placeholder">
                     <h1 tabindex="0">${this.greeting}</h1>
                     <h2 tabindex="0">di ${this.name}</h2>
                 </div>
