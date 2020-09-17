fab50c24be4a7fa2692c93d1582aed6742751675
Indra Mahkota <indramahkota1@gmail.com>
2020-09-17 19:52:37 +0700

    menonaktifkan parallax

--- src/scripts/_library_/components/hero-element/heroElement.ts
+++ src/scripts/_library_/components/hero-element/heroElement.ts
@@ -13,45 +13,6 @@ export default class HeroElement extends CommonElement {
     @property({ type: String, attribute: true })
     name = AppConfig.APP_NAME;
 
-    /*  #WARNING# Laggg... */
-    private _heroGreeting: HTMLElement | null = null;
-    private _heroButton: HTMLElement | null = null;
-    private _currScrollPos = 0;
-    private _ticking = false;
-
-    private _onScrollHandler = () => {
-        this._currScrollPos = window.scrollY;
-        if (!this._ticking) {
-            window.requestAnimationFrame(() => {
-                this.hideOrShowHeader();
-                this._ticking = false;
-            });
-            this._ticking = true;
-        }
-    }
-
-    hideOrShowHeader(): void {
-        if (this._heroGreeting === null || this._heroButton === null)
-            return;
-        this._heroGreeting.style.transform = `translateY(${(this._currScrollPos * 2) / 7}px)`;
-        this._heroButton.style.transform = `translateY(${this._currScrollPos / 3}px)`;
-    }
-
-    connectedCallback(): void {
-        super.connectedCallback();
-        window.addEventListener('scroll', this._onScrollHandler, false);
-    }
-
-    disconnectedCallback(): void {
-        window.removeEventListener('scroll', this._onScrollHandler, false);
-        super.disconnectedCallback();
-    }
-
-    firstUpdated(): void {
-        this._heroGreeting = document.getElementById('hero-greeting');
-        this._heroButton = document.getElementById('find-button');
-    }
-
     render(): TemplateResult {
         return html`
             <div class="hero__background">
@@ -64,7 +25,6 @@ export default class HeroElement extends CommonElement {
     }
 
     private _onButtonClickHandler(): void {
-        this._heroButton?.blur();
         const letsFind = new CustomEvent(EventType.LETS_FIND, {
             detail: {
                 message: `Let's Find`
