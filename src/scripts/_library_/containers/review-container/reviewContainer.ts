b02aab3faaabceed5aa1e86780593415f7fbb06e
Indra Mahkota <indramahkota1@gmail.com>
2020-09-18 22:49:03 +0700

    fix review style

--- src/scripts/_library_/containers/review-container/reviewContainer.ts
+++ src/scripts/_library_/containers/review-container/reviewContainer.ts
@@ -9,19 +9,18 @@ import '../../components/review-card/reviewCard';
 @customElement('review-container')
 export default class ReviewContainer extends CommonElement {
     @property({ type: String, attribute: true })
-    title = 'REVIEWS';
+    title = 'LATEST REVIEWS';
 
     @property({ type: Array, attribute: true })
     data?: ConsumerReview[] | null = null;
 
-    //.data=${res}
     render(): TemplateResult {
         return html`
             <div class="review__container">
                 <h1 tabindex="0" class="review__title">${this.title}</h1>
                     <div class="review__items">
                         ${
-                            this.data?.map(res => html`<review-card .data=${res}></review-card>`)
+                            this.data?.slice(Math.max(this.data?.length - 5, 0)).reverse().map(res => html`<review-card .data=${res}></review-card>`)
                         }
                     </div>
                 </div>
