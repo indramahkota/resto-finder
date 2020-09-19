a234a5e46a6e32278d806daecb6524dd6788a62f
Indra Mahkota <indramahkota1@gmail.com>
2020-09-19 22:38:13 +0700

    membetulkan submit form script

--- src/scripts/_library_/components/review-form/reviewForm.ts
+++ src/scripts/_library_/components/review-form/reviewForm.ts
@@ -1,36 +1,57 @@
 import { html, TemplateResult } from 'lit-html';
-import { customElement, property } from 'lit-element';
+import { customElement, internalProperty, property } from 'lit-element';
 
 import CommonElement from '../_base_/commonElement';
 
 import './review-form.scss';
+import EventType from '../../../globals/eventType';
 
 @customElement('review-form')
 export default class ReviewForm extends CommonElement {
-    @property({ type: String, attribute: true })
-    name = 'Reviewer';
+    @property({ type: Boolean, attribute: true })
+    complete = false;
 
-    @property({ type: String, attribute: true })
-    review = 'Review';
+    @internalProperty()
+    private _name = '';
+
+    @internalProperty()
+    private _review = '';
 
     private _onNameChangeHandler(event: Event): void {
-        this.name = (event.target as HTMLInputElement).value;
+        this._name = (event.target as HTMLInputElement).value;
     }
 
     private _onReviewChangeHandler(event: Event): void {
-        this.review = (event.target as HTMLTextAreaElement).value;
+        this._review = (event.target as HTMLTextAreaElement).value;
     }
 
     private _onButtonClickHandler(): void {
-        this.name = '';
-        this.review = '';
+        this.complete = false;
+        const submitReview = new CustomEvent(EventType.SUBMIT_REVIEW, {
+            detail: {
+                message: "Review Sumbitted",
+                data: {
+                    name: this._name,
+                    review: this._review
+                }
+            },
+            bubbles: true
+        });
+        this.dispatchEvent(submitReview);
+    }
+
+    updated(): void {
+        if (this.complete === true) {
+            this._name = '';
+            this._review = '';
+        }
     }
 
     render(): TemplateResult {
         return html`
             <div class="reviewform__container">
-                <input aria-label="Type your Name" class="reviewinput__name" placeholder="Type Your Name" type="text" @change="${this._onNameChangeHandler}">
-                <textarea aria-label="Type your Review" class="reviewtextarea__review" placeholder="This Restaurant is awesome!" @change="${this._onReviewChangeHandler}"></textarea>
+                <input aria-label="Type your Name" class="reviewinput__name" placeholder="Type Your Name" type="text" @change="${this._onNameChangeHandler}" .value="${this._name}">
+                <textarea aria-label="Type your Review" class="reviewtextarea__review" placeholder="This Restaurant is awesome!" @change="${this._onReviewChangeHandler}" .value="${this._review}"></textarea>
                 <button class="reviewbutton__submit" @click="${this._onButtonClickHandler}">Send Review</button>
             </div>
         `;
--- src/scripts/globals/eventType.ts
+++ src/scripts/globals/eventType.ts
@@ -2,5 +2,5 @@ export default class EventType {
     static readonly SHOW_TOAST: string = 'rstf::com.appToast.showToast';
     static readonly LETS_FIND: string = 'rstf::com.heroElement.letsFind';
     static readonly FAVORITE_CLICKED: string = 'rstf::com.favoriteButton.favoriteClicked';
-    static readonly REVIEW_SUBMITTED: string = 'rstf::com.reviewForm.reviewSubmitted';
+    static readonly SUBMIT_REVIEW: string = 'rstf::com.reviewForm.submitReview';
 }
