e5b275abe3136448bf9f3639c27dfd8a133c5ed8
Indra Mahkota <indramahkota1@gmail.com>
2020-09-19 21:03:52 +0700

    save change form

--- src/scripts/_library_/components/review-form/reviewForm.ts
+++ src/scripts/_library_/components/review-form/reviewForm.ts
@@ -8,14 +8,30 @@ import './review-form.scss';
 @customElement('review-form')
 export default class ReviewForm extends CommonElement {
     @property({ type: String, attribute: true })
-    message = 'This is Toaster';
+    name = 'Reviewer';
+
+    @property({ type: String, attribute: true })
+    review = 'Review';
+
+    private _onNameChangeHandler(event: Event): void {
+        this.name = (event.target as HTMLInputElement).value;
+    }
+
+    private _onReviewChangeHandler(event: Event): void {
+        this.review = (event.target as HTMLTextAreaElement).value;
+    }
+
+    private _onButtonClickHandler(): void {
+        this.name = '';
+        this.review = '';
+    }
 
     render(): TemplateResult {
         return html`
             <div class="reviewform__container">
-                <input aria-label="Type your Name" class="reviewinput__name" placeholder="Type Your Name" type="text">
-                <textarea aria-label="Type your Review" class="reviewtextarea__review" placeholder="This Restaurant is awesome!"></textarea>
-                <button class="reviewbutton__submit">Send Review</button>
+                <input aria-label="Type your Name" class="reviewinput__name" placeholder="Type Your Name" type="text" @change="${this._onNameChangeHandler}">
+                <textarea aria-label="Type your Review" class="reviewtextarea__review" placeholder="This Restaurant is awesome!" @change="${this._onReviewChangeHandler}"></textarea>
+                <button class="reviewbutton__submit" @click="${this._onButtonClickHandler}">Send Review</button>
             </div>
         `;
     }
