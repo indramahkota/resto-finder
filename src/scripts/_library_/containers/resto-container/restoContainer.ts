7dc705b1039db4ebddac45da2d3b725cd458b3a4
Indra Mahkota <indramahkota1@gmail.com>
2020-09-20 11:29:17 +0700

    menambahkan fitur data not found

--- src/scripts/_library_/containers/resto-container/restoContainer.ts
+++ src/scripts/_library_/containers/resto-container/restoContainer.ts
@@ -4,6 +4,7 @@ import { customElement, property } from 'lit-element';
 
 import { RestaurantResponse } from '../../../data/entity/RestaurantResponse';
 import CommonElement from '../../components/_base_/commonElement';
+import notFound from '../../../../assets/images/not-found.webp';
 
 import "../../components/resto-card/restoCard";
 import "../../components/restocard-shimmer/restoCardShimmer";
@@ -22,23 +23,35 @@ export default class RestoContainer extends CommonElement {
         return html`
             <div class="resto__container">
                 <h1 tabindex="0" class="resto__title">${this.title}</h1>
-                <div class="resto__items">
                 ${
-                        this.data !== null ?
-                            this.data.restaurants.map(res => html`
-                                <resto-card .data=${res}></resto-card>
-                            `) : html`
-                                <restocard-shimmer></restocard-shimmer>
-                                <restocard-shimmer></restocard-shimmer>
-                                <restocard-shimmer></restocard-shimmer>
-                                <restocard-shimmer></restocard-shimmer>
-                                <restocard-shimmer></restocard-shimmer>
-                                <restocard-shimmer></restocard-shimmer>
-                                <restocard-shimmer></restocard-shimmer>
-                                <restocard-shimmer></restocard-shimmer>
+                    this.data !== null ?
+                        this.data.restaurants.length > 0 ? html`
+                            <div class="resto__items">
+                                ${
+                                    this.data.restaurants.map(res => html`
+                                        <resto-card .data=${res}></resto-card>
+                                    `)
+                                }
+                            </div>
+                        ` : html`
+                            <div class="resto__empty">
+                                <img src="${notFound}">
+                                <p>No data found</p>
+                            </div>
+                        `
+                            : html`
+                                <div class="resto__items">
+                                    <restocard-shimmer></restocard-shimmer>
+                                    <restocard-shimmer></restocard-shimmer>
+                                    <restocard-shimmer></restocard-shimmer>
+                                    <restocard-shimmer></restocard-shimmer>
+                                    <restocard-shimmer></restocard-shimmer>
+                                    <restocard-shimmer></restocard-shimmer>
+                                    <restocard-shimmer></restocard-shimmer>
+                                    <restocard-shimmer></restocard-shimmer>
+                                </div>
                             `
-                    }
-                </div>
+                }
             </div>
         `;
     }
