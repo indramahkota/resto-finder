32f03eadb0c603e55327e288b53e730c25aa82e7
Indra Mahkota <indramahkota1@gmail.com>
2020-09-23 21:19:42 +0700

    fix notif dengan delay 3detik

--- src/sw.ts
+++ src/sw.ts
@@ -6,6 +6,8 @@ window.addEventListener("load", async () => {
 });
 
 window.addEventListener('DOMContentLoaded', async () => {
+    /* Belum sempat buat tombol untuk request notif menggunakan gesture */
+    await new Promise(res => setTimeout(res, 3000));
     await requestNotificationPermission();
     await getNotificationSubscription();
 });
