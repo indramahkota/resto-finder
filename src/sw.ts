da8d53e3ea312f08758aa3f09eee44fa3718d03c
Indra Mahkota <indramahkota1@gmail.com>
2020-09-23 21:29:20 +0700

    Mengembalikan fungsi notifikasi dan mengabaikan best practice

--- src/sw.ts
+++ src/sw.ts
@@ -3,11 +3,6 @@ import { getNotificationSubscription, requestNotificationPermission } from "./sc
 
 window.addEventListener("load", async () => {
     await register('./sw.js');
-});
-
-window.addEventListener('DOMContentLoaded', async () => {
-    /* Belum sempat buat tombol untuk request notif menggunakan gesture */
-    await new Promise(res => setTimeout(res, 3000));
     await requestNotificationPermission();
     await getNotificationSubscription();
 });
