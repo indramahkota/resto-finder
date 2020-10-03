4de90d2f7ce03168be8560c9b320b7bf99a21922
Indra Mahkota <indramahkota1@gmail.com>
2020-10-03 22:23:07 +0700

    konsisten resolve

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const path = require('path');
+const { resolve } = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
@@ -9,7 +9,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist')
+    contentBase: resolve(__dirname, 'dist')
   },
   module: {
     rules: [
